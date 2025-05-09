1cc2035b73a9eafd1a43183175c966fcb8526a26
brian-paiba <brianmanuelpaiba@outlook.es>
2025-05-09 10:12:42 -0300

    refactor: simplify page.tsx and add BandAdd and BandList components
    Remove unnecessary content from page.tsx to streamline the UI. Introduce BandAdd and BandList components to prepare for band management functionality.

---
+++ 03-bandnames/client/app/components/band-add.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BandAdd = () => {
+  return (
+    <div>BandAdd</div>
+  )
+}
---
+++ 03-bandnames/client/app/components/band-list.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BandList = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
